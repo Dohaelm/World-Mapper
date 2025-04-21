39cfc32fa61dd90193854b62fcc48bba00a29f68
nouhailapiro <nouhaila.p2003@gmail.com>
2025-04-21 17:26:19 +0100

    switching to openstreetmap

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,6 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices provideAPIKey:@"AIzaSyAzqhoRjvPt3mZ5BZ_MmG9wboup7Ls9k84"
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
---
+++ lib/pages/openStreetMap.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OpenstreetmapScreen extends StatefulWidget {
+  const OpenstreetmapScreen({super.key});
+
+  @override
+  State<OpenstreetmapScreen> createState() => _OpenStreetmapScreenState();
+}
+
+class _OpenStreetmapScreenState extends State<OpenstreetmapScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
